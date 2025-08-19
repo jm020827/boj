6c6a5970ec66234f1f2f29e971e40e3e4baf4796
jm020827 <jmsmlove02@snu.ac.kr>
2025-08-19 17:25:06 +0900

    [solved] 08.19

--- bronze/10871.cpp
+++ bronze/10871.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int n, x;
     cin >> n >> x;
     int a;
-    vector<int> result;
     for(int i = 0; i < n; i++) {
         cin >> a;
         if(a < x) {
-            result.push_back(a);
+            cout << a << " ";
         }
     }
-    for(int a : result) {
-        cout << a << " ";
-    }
-    
     return 0;
     
 }
@@ -27,6 +23,6 @@ int main()
 /*
 10871 -정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램
 
-입력을 받을 때마다 판단 후 result에 push_back 하여 입력받은 순서 그대로 출력하여 해결
+입력을 받을 때마다 판단 후 그대로 출력하여 해결
 
 */
